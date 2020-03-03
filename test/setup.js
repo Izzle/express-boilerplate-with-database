7e4def31e548b5f3ca38c7b1ea335c0d701c1fa9
Ron Martin <ronmartin89@gmail.com>
2020-03-03 09:10:42 -0800

    updated README.md and added some example code

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,7 @@
 // This file is run by our npm test, in package.json
 // A feature of mocha lets us require a specific setup file
+process.env.TZ = 'UTC';
+require('dotenv').config();
 const { expect } = require('chai');
 const supertest = require('supertest');
 
